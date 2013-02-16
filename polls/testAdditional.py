cfd7bda50bc6284a49f32efbaf992b9d40e4df51
Peter Lee <nadapeter@gmail.com>
2013-02-15 16:43:52 -0800

    added more test cases

--- polls/testAdditional.py
+++ polls/testAdditional.py
@@ -59,6 +59,25 @@ class TestAdditional(unittest.TestCase):
         """
         self.assertEquals(models.ERR_BAD_PASSWORD, self.users.addUser("user6", longString))
 
+    def testBadLogin(self):
+        
+        self.assertEquals(models.ERR_BAD_CREDENTIALS, self.users.loginUser("user7", "asdf"))
+
+    def testLogin(self):
+        
+        self.assertEquals(models.SUCCESS, self.users.addUser("user8", "asdf"))
+        self.assertEquals(2, self.users.loginUser("user8", "asdf"))
+
+    def testLoginCount(self):
+        self.assertEquals(models.SUCCESS, self.users.addUser("user8", "asdf"))
+        self.assertEquals(2, self.users.loginUser("user8", "asdf"))
+        self.assertEquals(3, self.users.loginUser("user8", "asdf"))
+        self.assertEquals(4, self.users.loginUser("user8", "asdf"))
+
+
+    def testBadLogin2(self):
+        
+        self.assertEquals(models.ERR_BAD_CREDENTIALS, self.users.loginUser("user8", "wrongpassword"))
 
 # If this file is invoked as a Python script, run the tests in this module
 if __name__ == "__main__":
